84ea98473081c2e47dfa89cc3a29d2ca672debbf
emnh <hvidevold@gmail.com>
2019-12-03 14:00:46 +0100

    2 files modified

--- index.js
+++ index.js
@@ -4,15 +4,28 @@ import './style.css';
 const PIXI = require('pixi.js');
 const TinyQueue = require('tinyqueue');
 const seedrandom = require('seedrandom');
+const randomInt = require('random-int');
 
 const randomGenerator = seedrandom('hello');
+
+const randInt = function(a, b) {
+  return randomInt(a, b);
+  // return a + Math.floor((b - a) * randomGenerator());
+};
+
+const clamp = function(x, a, b) {
+  return Math.min(Math.max(x, a), b);
+};
+
+// console.log(clamp(3, 1, 2));
 
 const Rectangle = function(x, y, width, height) {
   return {
     x: x,
     y: y,
     width: width,
-    height: height
+    height: height,
+    size: width * height
   };
 };
 
@@ -31,13 +44,13 @@ const splitRectangle = function(parent, child) {
     Rectangle(parent.x, child.y, child.x - parent.x, child.height),
     Rectangle(parent.x, child.y + child.height, child.x - parent.x, parent.height - (child.y + child.height)),
 
-    Rectangle(parent.x + child.x, parent.y, child.width, child.y - parent.y),
-    Rectangle(parent.x + child.x, child.y, child.width, child.height),
-    Rectangle(parent.x + child.x, child.y + child.height, child.width, parent.height - (child.y + child.height)),
+    Rectangle(child.x, parent.y, child.width, child.y - parent.y),
+    // Rectangle(child.x, child.y, child.width, child.height),
+    Rectangle(child.x, child.y + child.height, child.width, parent.height - (child.y + child.height)),
 
-    Rectangle(parent.x + child.x + child.width, parent.y, parent.width - (child.x + child.width), child.y - parent.y),
-    Rectangle(parent.x + child.x + child.width, child.y, parent.width - (child.x + child.width), child.height),
-    Rectangle(parent.x + child.x + child.width, child.y + child.height, parent.width - (child.x + child.width), parent.height - (child.y + child.height)),
+    Rectangle(child.x + child.width, parent.y, parent.width - (child.x + child.width), child.y - parent.y),
+    Rectangle(child.x + child.width, child.y, parent.width - (child.x + child.width), child.height),
+    Rectangle(child.x + child.width, child.y + child.height, parent.width - (child.x + child.width), parent.height - (child.y + child.height))
   ];
   return grid;
 };
@@ -60,39 +73,95 @@ const setup = function(state) {
 };
 
 const processing = function(state) {
+  state.graphics = new PIXI.Graphics();
+  state.stage.addChild(state.graphics);
+
   const before = performance.now();
   
-  const rectangles = [Rectangle(0, 0, state.width, state.height)];
+  const startRectangle = Rectangle(0, 0, state.width, state.height);
 
-  const child = Rectangle(100, 120, 150, 50);
+  const roomWidthMin = 20;
+  const roomWidthMax = 150;
+  const roomHeightMin = 20;
+  const roomHeightMax = 150;
 
-  const graphics = new PIXI.Graphics();
-  state.stage.addChild(graphics);
-
-  graphics.beginFill(getRandomColor());
-  graphics.drawRect(child.x, child.y, child.width, child.height);
-  graphics.endFill();
-
-  const firstSplits = splitRectangle(rectangles[0], child);
-  for (let i = 0; i < firstSplits.length; i++) {
-    const rect = firstSplits[i];
-    graphics.beginFill(state.colors[i]);
-    graphics.drawRect(rect.x, rect.y, rect.width, rect.height);
-    graphics.endFill();
-    rectangles.push(rect);
-  }
-
-  const roomCount = 100;
+  const roomCount = 2;
+  const rooms = [];
+  const splitQueue = new TinyQueue([startRectangle], (a, b) => b.size - a.size);
+  const addIndex = 0;
   for (let roomIndex = 0; roomIndex < roomCount; roomIndex++) {
+    const top = splitQueue.pop();
 
+    const minX = top.x;
+    const maxX = top.x + top.width;
+    const minY = top.y;
+    const maxY = top.y + top.height;
+    const x = randInt(minX, maxX);
+    const width = randInt(roomWidthMin, clamp(maxX - x, roomWidthMin, roomWidthMax));
+    const y = randInt(minY, maxY);
+    const height = randInt(roomHeightMin, clamp(maxY - y, roomHeightMin, roomHeightMax));
+
+    console.log("minX", minX, maxX, x, y, width, height);
+    const child = Rectangle(x, y, width, height);
+    // console.log(child.size);
+
+    state.graphics.beginFill(0xFFFFFF);
+    state.graphics.drawRect(child.x, child.y, child.width, child.height);
+    state.graphics.endFill();
+
+    const firstSplits = splitRectangle(top, child);
+    for (let i = 0; i < firstSplits.length; i++) {
+      const rect = firstSplits[i];
+
+      state.graphics.lineStyle(2, 0xFEEB77, 1);
+      let color = 0x000000;
+      switch (i) {
+        case 0:
+          color = 0xFF0000;
+          break;
+        case 1:
+          color = 0x800000;
+          break;
+        case 2:
+          color = 0x400000;
+          break;
+        case 3:
+          color = 0x00FF00;
+          break;
+        case 4:
+          color = 0x008000;
+          break;
+        case 5:
+          color = 0x0000FF;
+          break;
+        case 6:
+          color = 0x000080;
+          break;
+        case 7:
+          color = 0x000040;
+          break;
+      }
+      state.graphics.beginFill(color);
+      state.graphics.drawRect(rect.x, rect.y, rect.width, rect.height);
+      state.graphics.endFill();
+
+      rect.addIndex = addIndex;
+      addIndex++;
+
+      splitQueue.push(rect);
+    }
   }
-
   const after = performance.now();
+  state.elapsed = after - before;
+
+  return state;
+};
+
+const stats = function(state) {
   const p = document.createElement(p);
-  p.innerHTML = 'time: ' + Math.round(after - before) + 'ms';
+  p.innerHTML = 'time: ' + Math.round(state.elapsed) + 'ms';
   document.body.appendChild(document.createElement('br'));
   document.body.appendChild(p);
-
   return state;
 };
 
@@ -107,4 +176,5 @@ const startRenderLoop = function(state) {
 let state = {};
 state = setup(state);
 state = processing(state);
+state = stats(state);
 state = startRenderLoop(state);
